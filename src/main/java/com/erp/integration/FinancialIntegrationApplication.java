6107e581591fbd9c757f6741927acd1c1116a055
Supreeth A L <supreethal40@gmail.com>
2026-06-10 20:21:20 +0530

    Initial Spring Boot ERP integration project setup

---
+++ src/main/java/com/erp/integration/FinancialIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.erp.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/integration/FinancialIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
